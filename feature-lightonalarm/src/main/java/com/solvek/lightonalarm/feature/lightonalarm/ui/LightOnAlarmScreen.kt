7ed1784df852dc7d72581322a87b8840c45de3e4
solvek <adamchuk@gmail.com>
2022-12-29 17:22:54 +0200

    Fixed layout. More spaces.

--- feature-lightonalarm/src/main/java/com/solvek/lightonalarm/feature/lightonalarm/ui/LightOnAlarmScreen.kt
+++ feature-lightonalarm/src/main/java/com/solvek/lightonalarm/feature/lightonalarm/ui/LightOnAlarmScreen.kt
@@ -1,10 +1,16 @@
 package com.solvek.lightonalarm.feature.lightonalarm.ui
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
@@ -13,9 +19,9 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle.State.STARTED
 import androidx.lifecycle.repeatOnLifecycle
-import com.solvek.lightonalarm.feature.lightonalarm.ui.LightOnAlarmUiState.Success
 import com.solvek.lightonalarm.core.ui.MyApplicationTheme
 import com.solvek.lightonalarm.feature.lightonalarm.R
+import com.solvek.lightonalarm.feature.lightonalarm.ui.LightOnAlarmUiState.Success
 
 @Composable
 fun LightOnAlarmScreen(modifier: Modifier = Modifier, viewModel: LightOnAlarmViewModel = hiltViewModel()) {
@@ -49,18 +55,21 @@ internal fun LightOnAlarmScreen(
     onStopPlaying: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column(modifier) {
-        Row {
+    Column(modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Row (verticalAlignment = Alignment.CenterVertically){
             Checkbox(checked = isEnabled, onCheckedChange = onChangeAlarmSetting)
             Spacer(modifier = Modifier.size(6.dp))
             Text(stringResource(R.string.alarm_active))
         }
         if (isPlaying){
+            Spacer(Modifier.size(20.dp))
             Button(onClick = onStopPlaying) {
                 Text(stringResource(R.string.stop))
             }
         }
+        Spacer(Modifier.size(40.dp))
         Text(stringResource(R.string.help))
+        Spacer(Modifier.size(5.dp))
         Text(stringResource(R.string.stop_russians))
     }
 }
